98f84c8843a24da202ecb5f284086826d99d0317
henrique.roos <henriquemroos21@gmail.com>
2023-05-16 21:55:47 -0300

    feat: first commit

---
+++ src/theme/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1ed760",
+    },
+    secondary: {
+      main: "#222222",
+    },
+    success: {
+      main: "#1ed760",
+    },
+    background: {
+      default: "#222222",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#222222",
+    },
+  },
+});
+
+export default theme;
